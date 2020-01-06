cc0bb1b7b3c1aab4f4644ed9a4d535f8c74e79a1
Jesse Kuang <jkuang@21cn.com>
2020-01-06 16:19:00 +0800

    test EVM call input data

--- core/vm/runtime/bldewasm.sh
+++ core/vm/runtime/bldewasm.sh
@@ -1,28 +1,40 @@
 #! /bin/bash
 
 FILE=$1
-cat <<EOF > /tmp/${FILE}.cpp
-#include <stdint.h>
+cat <<EOF > /tmp/${FILE}.c
 
-extern "C" void finish(char* _off, int32_t _len) __attribute__((import_module("ethereum"), import_name("finish")));
+typedef	int	i32;
+typedef long long i64;
+typedef __int128_t i128;
+typedef	unsigned int	u32;
+typedef unsigned long long u64;
+typedef __uint128_t u128;
 
-/*
-extern "C"
-int64_t max(int64_t a, int64_t b) {
-   return a > b ? a: b;
-}
-*/
+void eth_finish(char* _off, i32 _len) __attribute__((import_module("ethereum"), import_name("finish")));
+i32  eth_getCallDataSize() __attribute__((import_module("ethereum"),import_name("getCallDataSize")));
+void eth_callDataCopy(void *res, i32 dOff, i32 dLen) __attribute__((import_module("ethereum"),import_name("callDataCopy")));
 
+// cpurchase   0xd6960697
+// creceived	0x73fac6f0
+// refund		0x590e1ae3
 static	char	ret[8]={0,0,0,0, 0,0,0,10};
-extern "C"
-void Main() //__attribute__((export_name("main")))
+static	char	ret1[8]={0,0,0,20};
+void main() // __attribute__((export_name("main")))
 {
-	finish(ret, 8);
+	if (eth_getCallDataSize() < 4) eth_finish(ret, 8);
+	u32 	met;
+	eth_callDataCopy(&met, 0, 4);
+	switch (__builtin_bswap32(met)) {
+	case 0xd6960697:
+	case 0x73fac6f0:
+	case 0x590e1ae3:
+		eth_finish(ret1,4);
+	}
 }
 EOF
 
-clang++ -c -O3 --target=wasm32 /tmp/${FILE}.cpp
-wasm-ld --no-entry --allow-undefined-file=ewasm.syms --export=Main --strip-all ${FILE}.o -o /tmp/${FILE}.wasm
+clang -c -O3 -Wall -Wno-main-return-type --target=wasm32 /tmp/${FILE}.c
+wasm-ld --no-entry --allow-undefined-file=ewasm.syms --export=main --strip-all ${FILE}.o -o ${FILE}.wasm
 rm -f ${FILE}.o
-wasm-dis /tmp/${FILE}.wasm | sed -s 's/Main/main/' > /tmp/${FILE}.wat
-wasm-as /tmp/${FILE}.wat -o ${FILE}.wasm
+#wasm-dis /tmp/${FILE}.wasm | sed -s 's/Main/main/' > /tmp/${FILE}.wat
+#wasm-as /tmp/${FILE}.wat -o ${FILE}.wasm
