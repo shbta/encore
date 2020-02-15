0b2c6c8b85bc660c8cf83f4d4ef79b6fbbb81c55
Jesse Kuang <jkuang@21cn.com>
2020-02-15 17:45:24 +0800

    update ewasm.cdt

--- fclear/bldewasm.sh
+++ fclear/bldewasm.sh
@@ -4,11 +4,13 @@
 # llvm 7 does not support import_module/import_name attributes
 # llvm 8 may export __heap_base and __data_end
 #
+# ewasm.syms ewasm.h should be installed under ~/opt/ewasm
+# bldewasm.sh under ~/bin could make use or librt
 
 FILE=$1
 
 clang -c -O3 -Wall -I${HOME}/opt/ewasm/include  --target=wasm32 ${FILE}.c
-wasm-ld --no-entry --allow-undefined-file=ewasm.syms --export=main --strip-all ${FILE}.o -o ${FILE}.wasm
+wasm-ld --no-entry --allow-undefined-file=${HOME}/opt/ewasm/ewasm.syms --export=main --strip-all ${FILE}.o -o ${FILE}.wasm
 rm -f ${FILE}.o
 #wasm-dis /tmp/${FILE}.wasm | sed -s 's/Main/main/' > /tmp/${FILE}.wat
 #wasm-as /tmp/${FILE}.wat -o ${FILE}.wasm
