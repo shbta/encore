b57be6cd4530a53365b70f54368472214abd8d00
RedTea@afternoon <3109688@qq.com>
2020-11-03 01:03:21 +0800

    works on encore ...

--- node/defaults.go
+++ node/defaults.go
@@ -55,30 +55,33 @@ var DefaultConfig = Config{
 
 // DefaultDataDir is the default data directory to use for the databases and other
 // persistence requirements.
+// Encore begin
 func DefaultDataDir() string {
 	// Try to place the data folder in the user's home dir
 	home := homeDir()
 	if home != "" {
 		switch runtime.GOOS {
 		case "darwin":
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "Encore")
 		case "windows":
 			// We used to put everything in %HOME%\AppData\Roaming, but this caused
 			// problems with non-typical setups. If this fallback location exists and
 			// is non-empty, use it, otherwise DTRT and check %LOCALAPPDATA%.
-			fallback := filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			fallback := filepath.Join(home, "AppData", "Roaming", "Encore")
 			appdata := windowsAppData()
 			if appdata == "" || isNonEmptyDir(fallback) {
 				return fallback
 			}
-			return filepath.Join(appdata, "Ethereum")
+			return filepath.Join(appdata, "Encore")
 		default:
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".encore")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
 	return ""
 }
+
+// Encore end
 
 func windowsAppData() string {
 	v := os.Getenv("LOCALAPPDATA")
